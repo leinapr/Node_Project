48a3a570ee3082c5e6fa8b0c7fd303ea249e7e80
leinapr <162492449+leinapr@users.noreply.github.com>
2024-12-29 19:13:49 +0100

    Update shop.component.ts

--- Node/ecommerce-project/frontend/ecommerce-frontend/src/app/shop/shop.component.ts
+++ Node/ecommerce-project/frontend/ecommerce-frontend/src/app/shop/shop.component.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss'],
   imports: [CommonModule, RouterModule],
   standalone: true,
 })
