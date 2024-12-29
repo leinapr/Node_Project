e9e7846cf559da4fb4d6bf143423887d00aee77d
leinapr <162492449+leinapr@users.noreply.github.com>
2024-12-29 19:01:07 +0100

    Update app.routes.ts

--- Node/ecommerce-project/frontend/ecommerce-frontend/src/app/app.routes.ts
+++ Node/ecommerce-project/frontend/ecommerce-frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ShopComponent } from './shop/shop.component';
+import { MangaStatsComponent } from './manga-stats/manga-stats.component';
 import { CartComponent } from './cart/cart.component';
 import { SproductComponent } from './sproduct/sproduct.component';
 import { AdminComponent } from './admin/admin.component';
@@ -11,6 +12,7 @@ export const routes: Routes = [
   { path: '', component: MainPageComponent },
   { path: 'about', component: AboutComponent },
   { path: 'shop', component: ShopComponent },
+  { path: 'stats', component: MangaStatsComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'cart', component: CartComponent },
   { path: 'sproduct', component: SproductComponent },
